22d71e9804a1d543114708edbd28fcbae1e561d4
Andrew Rivett <veggiefrog@gmail.com>
2015-01-18 16:44:18 -0500

    Moved uiSettings into hooks

---
+++ hooks/uiSettings.php
@@ -0,0 +1,30 @@
+<?php
+//
+// Description
+// -----------
+// This function will return a list of user interface settings for the module.
+//
+// Arguments
+// ---------
+// ciniki:
+// business_id:		The ID of the business to get events for.
+//
+// Returns
+// -------
+//
+function ciniki_bugs_hooks_uiSettings($ciniki, $business_id, $args) {
+
+	$settings = array();
+
+	//
+	// Get the settings
+	//
+	$rc = ciniki_core_dbDetailsQueryDash($ciniki, 'ciniki_bugs_settings', 'business_id', 
+		$business_id, 'ciniki.bugs', 'settings', '');
+	if( $rc['stat'] == 'ok' && isset($rc['settings']) ) {
+		$settings = $rc['settings'];
+	}
+
+	return array('stat'=>'ok', 'settings'=>$settings);	
+}
+?>
