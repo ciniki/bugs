e414fa847cdb9974a94c80bc961d251acfccb290
Andrew Rivett <veggiefrog@gmail.com>
2016-09-10 10:26:59 -0400

    Updated hooks/uiSettings for new business menu

--- hooks/uiSettings.php
+++ hooks/uiSettings.php
@@ -7,14 +7,17 @@
 // Arguments
 // ---------
 // ciniki:
-// business_id:     The ID of the business to get events for.
+// business_id:     The ID of the business to get bugs for.
 //
 // Returns
 // -------
 //
 function ciniki_bugs_hooks_uiSettings($ciniki, $business_id, $args) {
 
-    $settings = array();
+    //
+    // Setup the default response
+    //
+    $rsp = array('stat'=>'ok', 'settings'=>array(), 'menu_items'=>array());
 
     //
     // Get the settings
@@ -22,9 +25,27 @@ function ciniki_bugs_hooks_uiSettings($ciniki, $business_id, $args) {
     $rc = ciniki_core_dbDetailsQueryDash($ciniki, 'ciniki_bugs_settings', 'business_id', 
         $business_id, 'ciniki.bugs', 'settings', '');
     if( $rc['stat'] == 'ok' && isset($rc['settings']) ) {
-        $settings = $rc['settings'];
+        $rsp['settings'] = $rc['settings'];
     }
 
-    return array('stat'=>'ok', 'settings'=>$settings);  
+    //
+    // Check permissions for what menu items should be available
+    //
+    if( isset($ciniki['business']['modules']['ciniki.bugs'])
+        && (isset($args['permissions']['owners'])
+            || isset($args['permissions']['employees'])
+            || isset($args['permissions']['resellers'])
+            || ($ciniki['session']['user']['perms']&0x01) == 0x01
+            )
+        ) {
+        $menu_item = array(
+            'priority'=>5300,
+            'label'=>'Bug Tracking', 
+            'edit'=>array('app'=>'ciniki.bugs.main'),
+            );
+        $rsp['menu_items'][] = $menu_item;
+    } 
+
+    return $rsp;
 }
 ?>
