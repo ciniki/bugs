8379285ce89cb2d59c4a79182960fe3df0fa3949
Andrew Rivett <veggiefrog@gmail.com>
2016-09-25 16:05:29 -0400

    Updated uiSettings for settings_menu_items

--- hooks/uiSettings.php
+++ hooks/uiSettings.php
@@ -17,13 +17,12 @@ function ciniki_bugs_hooks_uiSettings($ciniki, $business_id, $args) {
     //
     // Setup the default response
     //
-    $rsp = array('stat'=>'ok', 'settings'=>array(), 'menu_items'=>array());
+    $rsp = array('stat'=>'ok', 'settings'=>array(), 'menu_items'=>array(), 'settings_menu_items'=>array());
 
     //
     // Get the settings
     //
-    $rc = ciniki_core_dbDetailsQueryDash($ciniki, 'ciniki_bugs_settings', 'business_id', 
-        $business_id, 'ciniki.bugs', 'settings', '');
+    $rc = ciniki_core_dbDetailsQueryDash($ciniki, 'ciniki_bugs_settings', 'business_id', $business_id, 'ciniki.bugs', 'settings', '');
     if( $rc['stat'] == 'ok' && isset($rc['settings']) ) {
         $rsp['settings'] = $rc['settings'];
     }
@@ -44,7 +43,17 @@ function ciniki_bugs_hooks_uiSettings($ciniki, $business_id, $args) {
             'edit'=>array('app'=>'ciniki.bugs.main'),
             );
         $rsp['menu_items'][] = $menu_item;
+
     } 
+
+    if( isset($ciniki['business']['modules']['ciniki.bugs'])
+        && (isset($args['permissions']['owners'])
+            || isset($args['permissions']['resellers'])
+            || ($ciniki['session']['user']['perms']&0x01) == 0x01
+            )
+        ) {
+        $rsp['settings_menu_items'][] = array('priority'=>5300, 'label'=>'Bug Tracker', 'edit'=>array('app'=>'ciniki.bugs.settings'));
+    }
 
     return $rsp;
 }
